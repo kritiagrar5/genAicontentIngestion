972e28cc2df1a3182817baba96c2c7c65bc91df8
Dorian Li <dorian.li@sap.com>
2025-10-15 13:45:55 +0800

    metadata1

--- srv/service.js
+++ srv/service.js
@@ -142,7 +142,10 @@ this.on('READ', 'Banks', async (req) => {
     if (oneFile.fileType === "Standard Account Line Mapping") {
       //parse the xlsx file and update the metadatatable, first row is header
       const xlsx = require("xlsx");
-      const buffer = oneFile.content;
+      const buffer = Buffer.from(oneFile.content);
+      console.log("typeof oneFile.content",typeof oneFile.content); // e.g., 'string', 'object', etc.
+      console.log(oneFile.content && oneFile.content.constructor && oneFile.content.constructor.name);
+      console.log(String(oneFile.content).slice(0, 100)); // Print a snippet
       console.log('file content is Buffer:',Buffer.isBuffer(oneFile.content));
       const workbook = xlsx.read(buffer, { type: "buffer" });
       console.log('workbook is: ',workbook);
